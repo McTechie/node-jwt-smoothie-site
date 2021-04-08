85cd102114211c14d95bb5c13414556a856e8134
Mcvean Soans <mc.soans02@gmail.com>
2021-04-08 14:03:32 +0530

    add: Mongoose Validation for new signup

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,26 @@
 const User = require('../models/User');
 
+// handle errors
+const handleErrors = (err) => {
+    // console.log(err.message, err.code);
+    let errors = { email: '', password: '' };
+
+    // duplicate email error using code
+    if (err.code === 11000) {
+        errors.email = "That email has already been registered";
+        return errors;
+    }
+
+    // validation errors
+    if (err.message.includes('user validation failed')) {
+        Object.values(err.errors).forEach( ({ properties}) => {
+            errors[properties.path] = properties.message;
+        })
+    }
+
+    return errors;
+}
+
 module.exports.signup_get = (req,res) => {
     res.render('signup');
 }
@@ -15,8 +36,8 @@ module.exports.signup_post = async (req, res) => {
         const user = await User.create({ email, password });
         res.status(201).json(user);
     } catch (err) {
-        console.log(err);
-        res.status(404).send('error, user not created');
+        const errors = handleErrors(err);
+        res.status(404).json({ errors });
     }
 }
 
